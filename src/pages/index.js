5bc7c35f9366e3875cedde0e51d784a768e1cf81
Avais <avaisqurni@gmail.com>
2022-10-27 12:36:29 +0100

    acg site commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ const IndexPage = () => {
 
           <footer className='footer'>
             <a
-              href="https://avaizur.com"
+              href="https://acloud.guru"
             >
               Powered by{' '}
               <span className='logo'>
